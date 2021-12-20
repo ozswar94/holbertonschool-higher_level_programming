93a7c3a3e4cae561c4a1295d9e5309a78991c970
ozswar94 <3630@holbertonschool.com>
2021-12-20 12:11:15 +0000

    fix

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
 word = "Holberton"
-
-print("First 3 letters: {}".format(word[:3]))
-print("Last 2 letters: {}".format(word[-2:]))
-print("Middle word: {}".format(word[1:8]))
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:8]
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
