b3795483348d670cf0f26dda24851e3bc1f950b6
ozswar94 <3630@holbertonschool.com>
2021-12-23 11:40:27 +0000

    test 1 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,13 @@
 
 import sys
 
-nb_argv = len(sys.argv)
+if __name__ == "__main__":
+    nb_argv = len(sys.argv)
 
-if nb_argv == 2:
-    print("1 argument:")
-else:
-    print("{:d} arguments:".format(nb_argv))
+    if nb_argv == 2:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(nb_argv - 1))
 
-for i in range(1, nb_argv):
-    print("{:d}: {:s}".format(i, sys.argv[i]))
+    for i in range(1, nb_argv):
+        print("{:d}: {:s}".format(i, sys.argv[i]))
