3f7413ee32050b97cfc48de513f07fed2bad71a9
ozswar94 <3630@holbertonschool.com>
2022-01-03 13:53:50 +0000

    fix task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    size_tuple_a = len(tuple_a)
-    size_tuple_b = len(tuple_b)
     somme = [0, 0]
+    size = 0
     for i in range(len(tuple_a)):
-        somme[i] += tuple_a[i]
+        if size < 2:
+            somme[i] += tuple_a[i]
+            size += 1
+    size = 0
     for i in range(len(tuple_b)):
-        somme[i] += tuple_b[i]
+        if size < 2:
+            somme[i] += tuple_b[i]
+            size += 1
     somme = tuple(somme)
     return somme
