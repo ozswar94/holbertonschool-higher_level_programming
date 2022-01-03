901cd0d6369db3408d62bb736b01d59543d52745
ozswar94 <3630@holbertonschool.com>
2022-01-03 13:07:53 +0000

    fix 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,8 +3,8 @@ def no_c(my_string):
     if not my_string:
         return my_string
     my_string = list(my_string)
-    for c in my_string:
-        if c in 'cC':
-            my_string.remove(c)
+    for i in range(len(my_string)):
+        if my_string[i] in 'cC':
+            my_string[i] = ''
     my_string = ''.join(my_string)
     return my_string
