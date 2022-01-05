cefd200c4ecb5a87239f40f760fdd2a33c021de0
ozswar94 <3630@holbertonschool.com>
2022-01-05 11:13:39 +0000

    fix uniq

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     sum_list = 0
+    uniq_list = []
     for i in my_list:
-        sum_list += i
+        if i not in uniq_list:
+            sum_list += i
+            uniq_list.append(i)
     return sum_list
