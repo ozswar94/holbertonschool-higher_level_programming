9f2cd2589c96215e5fbd2bb8a351682df3c3a8b4
ozswar94 <3630@holbertonschool.com>
2022-01-05 22:06:05 +0000

    task 100 fix empty list

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if not my_list:
-        return None
+        return 0
     result = 0
     quotient = 0
     for i in my_list:
         result += i[0] * i[1]
         quotient += i[1]
-    return float(result / quotient)
+    return result / quotient
