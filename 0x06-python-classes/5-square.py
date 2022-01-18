64eaf94a222899a8348cf792313fd03cf8f24119
ozswar94 <3630@holbertonschool.com>
2022-01-18 17:06:22 +0000

    add comment

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+
+" A class Square define "
+
+
 class Square:
+    """
+    Square class
+
+    Attributes:
+        size (int): size of the square, private attribute
+    """
     def __init__(self, size=0):
             self.size = size
 
