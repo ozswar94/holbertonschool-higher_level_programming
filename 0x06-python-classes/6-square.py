8ed19db32ee2eb088b2cfef91b8c0f10f6dbfeff
ozswar94 <3630@holbertonschool.com>
2022-01-18 19:41:57 +0000

    fix error

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -53,7 +53,7 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
-            return
-        print("\n" * self.__position[1], end="")
+        for i in range(self.__position[1]):
+            print()
         for i in range(self.__size):
             print((" " * self.__position[0]) + ("#" * self.__size))
