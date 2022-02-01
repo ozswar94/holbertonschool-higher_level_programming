50f3438bc55daf2c1a41b560155ac1fe922cedd1
ozswar94 <3630@holbertonschool.com>
2022-02-01 17:29:15 +0000

    fix 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,10 +4,5 @@
 
 def read_file(filename=""):
     """open file in utf-8 and print"""
-    try:
-        with open(filename, 'r', encoding='utf8') as f:
-                print(f.read(), end="")
-    except FileNotFoundError:
-        print("file doesn't exist")
-    except PermissionError:
-        print("file permission")
+    with open(filename, 'r', encoding='utf8') as f:
+        print(f.read(), end="")
