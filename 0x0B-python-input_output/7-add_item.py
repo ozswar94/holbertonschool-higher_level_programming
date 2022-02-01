b595017ccc3ea868c13ab8b906b06472cd21b4cf
ozswar94 <3630@holbertonschool.com>
2022-02-01 18:34:25 +0000

    fix json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,7 +9,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 try:
     obj = load_from_json_file("add_item.json")
-except:
+except FileNotFoundError:
     obj = []
 
 if len(sys.argv) > 1:
