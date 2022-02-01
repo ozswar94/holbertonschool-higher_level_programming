6f3aa913f04ac70eb2a92f76cf311ffc9a838c59
ozswar94 <3630@holbertonschool.com>
2022-02-01 18:24:22 +0000

    test 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,11 +9,12 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
-if __name__ == '__main__':
-    try:
-        obj = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        obj = []
+try:
+    obj = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    obj = []
+
+if argc > 1:
     for i in range(1, sys.argc):
         obj.append(argv[i])
     save_to_json_file(obj, "add_item.json")
