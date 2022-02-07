367c16c2be2cd3f66959ca1de3ed29d4855b3224
ozswar94 <3630@holbertonschool.com>
2022-02-07 13:28:04 +0000

    test 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,7 +22,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ return Json representation """
-        if not list_dictionaries and len(list_dictionaries) == 0:
+        if not list_dictionaries or list_dictionaries is None:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
