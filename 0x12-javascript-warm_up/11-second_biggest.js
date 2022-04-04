8f962a616a43d07b1bd30756c4d295cd3b9012e2
Ozswar <jordan6heat@hotmail.fr>
2022-04-04 18:15:14 +0200

    add 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const tab = [];
+  process.argv.forEach(function (items) {
+    if (Number(items)) { tab.push(Number(items)); }
+  });
+
+  tab.sort();
+  console.log(tab[tab.length - 2]);
+}
