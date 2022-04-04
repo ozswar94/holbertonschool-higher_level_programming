e96fe25a18ff48eac88d8948df678793334fe9df
Ozswar <jordan6heat@hotmail.fr>
2022-04-04 15:47:49 +0200

    fix error for string + int

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 
-const squareSize = parseInt(process.argv[2]);
-
 if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < squareSize; i++) {
-    console.log('x'.repeat(squareSize));
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log('x'.repeat(process.argv[2]));
   }
 }
