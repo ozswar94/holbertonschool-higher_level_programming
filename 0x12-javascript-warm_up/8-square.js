b21543622015fbc8888202dea5e1a2b729348690
Ozswar <jordan6heat@hotmail.fr>
2022-04-04 15:53:04 +0200

    fix char print

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,6 +4,6 @@ if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < process.argv[2]; i++) {
-    console.log('x'.repeat(process.argv[2]));
+    console.log('X'.repeat(process.argv[2]));
   }
 }
