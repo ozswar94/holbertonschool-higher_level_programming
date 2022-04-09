1918df2288dd7b563a3c358be41f128150c3a468
Jordan Mpounza <3630@holbertonschool.com>
2022-04-09 08:26:04 +0000

    add project 0x0F

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import sys
+import MySQLdb
+
+
+db = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    db=sys.argv[3]
+    )
+curs = db.cursor()
+curs.execute("SELECT * FROM states ORDER BY id ASC")
+for row in curs.fetchall():
+    print(row)
+curs.close()
+db.close()
