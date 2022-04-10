3e65942d3ad714b8718307f64b7816365c32f4d9
Jordan Mpounza <3630@holbertonschool.com>
2022-04-10 08:28:14 +0000

    add comment

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
+"""
+lists all states from the database hbtn_0e_0_usa where name egal name of filed
+
+Arguments:
+    mysql username sys.argv[1]
+    mysql password sys.argv[2]
+    database name sys.argv[3]
+    name of filed sys.argv[4]
+"""
 
 import sys
 import MySQLdb
 
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3]
-    )
-curs = db.cursor()
-curs.execute("SELECT * FROM states WHERE name='{}'".format(sys.argv[4]))
-for row in curs.fetchall():
-    print(row)
-curs.close()
-db.close()
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
+    curs = db.cursor()
+    curs.execute("SELECT * FROM states WHERE name='{}'".format(sys.argv[4]))
+    for row in curs.fetchall():
+        print(row)
+    curs.close()
+    db.close()
