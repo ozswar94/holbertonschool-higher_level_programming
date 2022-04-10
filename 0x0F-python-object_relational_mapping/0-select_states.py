de16711fbe5b5ff97d8ef727d32cb17c14c9766d
Jordan Mpounza <3630@holbertonschool.com>
2022-04-10 08:07:33 +0000

    pycodesty test

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-
+"""
+list table
+"""
 import sys
 import MySQLdb
 
-
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3]
-    )
-curs = db.cursor()
-curs.execute("SELECT * FROM states ORDER BY id ASC")
-for row in curs.fetchall():
-    print(row)
-curs.close()
-db.close()
+if __name__ = '__main__': 
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
+    curs = db.cursor()
+    curs.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in curs.fetchall():
+        print(row)
+    curs.close()
+    db.close()
