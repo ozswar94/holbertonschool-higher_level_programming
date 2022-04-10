20875462f07c69c304727890ca0e224e146ace18
Jordan Mpounza <3630@holbertonschool.com>
2022-04-10 08:17:05 +0000

    pycode style fix 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
+"""
+lists all states from the database hbtn_0e_0_usa where name start with N
+
+Arguments:
+    mysql username sys.argv[1]
+    mysql password sys.argv[2]
+    database name sys.argv[3]
+"""
 
 import sys
 import MySQLdb
 
-
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3]
-    )
-curs = db.cursor()
-curs.execute("SELECT * FROM states WHERE name REGEXP '^[N].*'")
-for row in curs.fetchall():
-    print(row)
-curs.close()
-db.close()
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
+    curs = db.cursor()
+    curs.execute("SELECT * FROM states WHERE name REGEXP '^[N].*'")
+    for row in curs.fetchall():
+        print(row)
+    curs.close()
+    db.close()
