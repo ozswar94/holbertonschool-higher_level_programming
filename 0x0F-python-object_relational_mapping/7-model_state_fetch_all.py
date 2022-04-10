d7851bbc4cb7dfe08fa70b720853cd403befe253
Jordan Mpounza <3630@holbertonschool.com>
2022-04-10 12:06:01 +0000

    fix error checker all() -> first()

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -23,6 +23,6 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    for state in session.query(State).order_by(State.id).all():
+    state = session.query(State).order_by(State.id).first():
         print("{}: {}".format(state.id, state.name))
     session.close()
