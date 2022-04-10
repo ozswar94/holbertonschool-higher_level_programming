d24bd3fc7a718403c67b4ae48d3520e0d37c9243
Jordan Mpounza <3630@holbertonschool.com>
2022-04-10 08:44:00 +0000

    add task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
+"""
+lists all cities from the database hbtn_0e_0_usa
+fix injection
+
+Arguments:
+    mysql username sys.argv[1]
+    mysql password sys.argv[2]
+    database name sys.argv[3]
+"""
 
 import sys
 import MySQLdb
 
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3]
-    )
-curs = db.cursor()
-curs.execute(
-    """
-    SELECT * FROM cities
-    """
-    )
-for row in curs.fetchall():
-    print(row)
-curs.close()
-db.close()
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
+    curs = db.cursor()
+    curs.execute(
+        """
+        SELECT * FROM cities ORDER BY id ASC
+        """
+        )
+    for row in curs.fetchall():
+        print(row)
+    curs.close()
+    db.close()
