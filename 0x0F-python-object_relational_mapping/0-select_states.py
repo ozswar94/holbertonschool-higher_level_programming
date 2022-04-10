1240ab686ab3d15a5d66d64ec896264330947600
Jordan Mpounza <3630@holbertonschool.com>
2022-04-10 08:12:30 +0000

    pycodesty test

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 """
-list table
+lists all states from the database hbtn_0e_0_usa
+
+Arguments:
+    mysql username sys.argv[1]
+    mysql password sys.argv[2]
+    database name sys.argv[3]
 """
+
 import sys
 import MySQLdb
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
