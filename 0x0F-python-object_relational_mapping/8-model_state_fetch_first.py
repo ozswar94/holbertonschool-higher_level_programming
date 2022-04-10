a656a82c0d936da79034e7bb46a9656c75baf67f
Jordan Mpounza <3630@holbertonschool.com>
2022-04-10 11:52:52 +0000

    add task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+lists all state from the database hbtn_0e_0_usa using ORM
+
+Arguments:
+    mysql username sys.argv[1]
+    mysql password sys.argv[2]
+    database name sys.argv[3]
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import (create_engine)
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3],
+        pool_pre_ping=True
+        ))
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    state = session.query(State).order_by(State.id).all()
+    print("{}: {}".format(state[0].id, state[0].name))
+    session.close()
