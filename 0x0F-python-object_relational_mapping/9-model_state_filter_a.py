ca4a840b2acd802b3bc8a0207b929582a6d5ba20
Jordan Mpounza <3630@holbertonschool.com>
2022-04-10 12:51:35 +0000

    add 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+lists the first state from the database hbtn_0e_0_usa using ORM
+
+Arguments:
+    mysql username sys.argv[1]
+    mysql password sys.argv[2]
+    database name sys.argv[3]
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import (create_engine)
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3],
+        pool_pre_ping=True
+        ))
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id).all():
+        if 'a' in state.name:
+            print("{}: {}".format(state.id, state.name))
+    session.close()
