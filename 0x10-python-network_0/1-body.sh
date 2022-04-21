1a172069b1a521f382a8ca1caf919a60a342ea30
Ozswar <jordan6heat@hotmail.fr>
2022-04-21 12:24:04 +0000

    test -L

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # display if success
-http_response=$(curl -X GET -s -o body.txt -w "%{http_code}" "$1"); if [ "$http_response" -eq 200 ]; then cat body.txt; fi ; rm body.txt
+curl -X GET -s -L "$1"
