8bfd6e21e3e983d8fc266e056394b62369c38a90
Ozswar <jordan6heat@hotmail.fr>
2022-04-21 13:50:28 +0000

    test 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # list method allow
-curl -s -d "email=test@gmail.com" -d "subject=I will always be here for PLD" -X POST "$1"
+curl -sX POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
