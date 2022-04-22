f9bb0e728e254bb5a59d7c29e5a33e38eca18403
ozswar94 <3630@holbertonschool.com>
2022-04-22 11:21:42 +0000

    add task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+from urllib import request, parse
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+if __name__ == '__main__':
+    value = {'email': email}
+    value = parse.urlencode(value).encode('utf-8')
+    req = request.Request(url, value)
+    with request.urlopen(req) as reponse:
+        print(reponse.read().decode('utf-8'))
